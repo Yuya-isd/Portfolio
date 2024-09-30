4f25c71b1c983f3658f2b9b1f9bf69e1032f494c
Yuya-isd <ishida.yuya2022@gmail.com>
2024-09-30 23:21:44 +0900

    Add files via upload

---
+++ SingletonObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonObject : MonoBehaviour
+{
+    private static GameObject mInstance;
+
+    //Don't DestroyしているオブジェクトのInstance状態を返す関数
+    public static GameObject GetInstance()
+    {
+        return mInstance;
+    }
+
+    void Awake()
+    {
+        if(mInstance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            mInstance = gameObject;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
